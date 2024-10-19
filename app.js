8b4675b66ecad7bdbaae7ebbf3e5aaa8f2c1ee44
Anubhav Singh <anubhavsinghgtm@gmail.com>
2024-10-19 13:13:18 +0530

    env is added

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
+const port  = process.env.PORT || 8081;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Vercel!' });
+  res.send({ message: 'Hello from Vercel!  this' });
 });
 
+app.listen(port, ()=>{
+    console.log("listening at ", port)
+})
 
-module.exports = app;
+
+// module.exports = app;
