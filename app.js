823d87f2f4460aa6d6102b8fc823e92187f9981d
Anubhav Singh <anubhavsinghgtm@gmail.com>
2024-10-19 13:19:48 +0530

    mongo added

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const port  = process.env.PORT || 8081;
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB Atlas'))
+.catch(err => console.error('Could not connect to MongoDB:', err));
+
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello from Vercel!  this' });
